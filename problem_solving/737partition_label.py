75fa6c8b1def6f2a518caea925a92dffae121a04
Bazarovay <rxs6754@rit.edu>
2018-03-19 23:56:31 -0400

    using greedy approach

--- problem_solving/737partition_label.py
+++ problem_solving/737partition_label.py
@@ -1,12 +1,24 @@
 class Solution(object):
     def partitionLabels(self, S):
-        last = {c: i for i, c in enumerate(S)}
-        j = anchor = 0
-        ans = []
-        for i, c in enumerate(S):
-            j = max(j, last[c])
-            if i == j:
-                ans.append(i - anchor + 1)
-                anchor = i + 1
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        last_pos = {val: index for index, val in enumerate(S)}
 
-        return ans
+        min_len = -1
+        division_array = []
+        last_index = 0
+        for x_i in range(0,len(S)):
+
+            last_pos_index = last_pos[S[x_i]]
+
+            if last_pos_index > min_len:
+                min_len = last_pos_index
+
+            if x_i == min_len:
+                division_array.append(min_len + 1 - last_index)
+                last_index = min_len + 1
+                min_len = -1
+
+        return(division_array)
